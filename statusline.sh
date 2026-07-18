15c3a710139a64613ee5d10b49c121dd2374f971
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2026-07-18 15:01:19 +0200

    generated files on 2026-07-18

--- statusline.sh
+++ statusline.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 input=$(cat)
-PCT=$(echo "$input" | jq -r '.context_window.used_percentage // 0' | cut -d. -f1)
 
-printf '\033[1;31m[\033[1;33m%s\033[1;32m@\033[1;34m%s \033[1;35m%s\033[1;31m]\033[0m %s%% context' "$(whoami)" "$(hostname)" "$(pwd)" "$PCT"
+IN_TOKENS=$(echo "$input" | jq -r '.context_window.total_input_tokens // 0')
+OUT_TOKENS=$(echo "$input" | jq -r '.context_window.total_output_tokens // 0')
+TOTAL_TOKENS=$((IN_TOKENS + OUT_TOKENS))
+
+printf '\033[1;31m[\033[1;33m%s\033[1;32m@\033[1;34m%s \033[1;35m%s\033[1;31m]\033[0m %s tokens' "$(whoami)" "$(hostname)" "$(pwd)" "$TOTAL_TOKENS"
